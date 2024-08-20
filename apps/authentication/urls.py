bb9088b55766dfcf4a4730a22e32a5724a897526
Willtzar <wilimf02@gmail.com>
2024-08-20 14:52:07 -0500

    Configuracion para permisos y usuarios

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path,include
 from rest_framework import routers
 from apps.authentication import views
+from rest_framework.documentation import include_docs_urls
 
 
 #router=routers.DefaultRouter()
@@ -18,4 +19,5 @@ from .views import LoginAPIView
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='api_login'),
+    path("docs/",include_docs_urls(title="Centro medico APIS")),
 ]
--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("tasks/", include(r"tasks.urls")),
     path("auth/", include(r"apps.authentication.urls")),
 ]
